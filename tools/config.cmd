d4e51e585f44961ad9955e6542983032a0687897
K Lauer <klauer@slac.stanford.edu>
2020-02-12 10:33:04 -0800

    CLN: using both '@echo off' and '@' is unnecessary

--- tools/config.cmd
+++ tools/config.cmd
@@ -1,46 +1,46 @@
 @echo off
-@SET DockerImage=pcdshub/ads-deploy:latest
+SET DockerImage=pcdshub/ads-deploy:latest
 
-@SET DeployRoot=%~dp0
-@SET SolutionDir=%1
-@SET SolutionFilename=%2
+SET DeployRoot=%~dp0
+SET SolutionDir=%1
+SET SolutionFilename=%2
 
-@SET SolutionDir=%SolutionDir:~1,-1%
-@SET SolutionFilename=%SolutionFilename:~1,-1%
-@SET SolutionName=%SolutionFilename:~0,-4%
+SET SolutionDir=%SolutionDir:~1,-1%
+SET SolutionFilename=%SolutionFilename:~1,-1%
+SET SolutionName=%SolutionFilename:~0,-4%
 
-@IF [%SolutionDir:~-2%]==[\\] (
-    @SET SolutionDir=%SolutionDir:~0,-1%
+IF [%SolutionDir:~-2%]==[\\] (
+    SET SolutionDir=%SolutionDir:~0,-1%
 )
 
-@SET SolutionFullPath="%SolutionDir%%SolutionName%"
-@SET IocMountPath=/reg/g/pcds/epics/ioc/%SolutionName%
+SET SolutionFullPath="%SolutionDir%%SolutionName%"
+SET IocMountPath=/reg/g/pcds/epics/ioc/%SolutionName%
 
-@if not [%AdsDeployConfigured%] == [1] (
-	@echo ----------------------------------------------------------------------
-	@echo Solution directory:      %SolutionDir%
-	@echo Solution name:           %SolutionName%
-	@echo Solution filename:       %SolutionFilename%
-	@echo Solution full path:      %SolutionFullPath%
-	@echo IOC mount path:          %IocMountPath%
-	@echo ----------------------------------------------------------------------
-	@echo.
+if not [%AdsDeployConfigured%] == [1] (
+	echo ----------------------------------------------------------------------
+	echo Solution directory:      %SolutionDir%
+	echo Solution name:           %SolutionName%
+	echo Solution filename:       %SolutionFilename%
+	echo Solution full path:      %SolutionFullPath%
+	echo IOC mount path:          %IocMountPath%
+	echo ----------------------------------------------------------------------
+	echo.
 )
 
-@SET ConfigSuccess=1
-@IF "%SolutionDir%" == "" (
-	@echo Solution directory unset!
-	@SET ConfigSuccess=0
+SET ConfigSuccess=1
+IF "%SolutionDir%" == "" (
+	echo Solution directory unset!
+	SET ConfigSuccess=0
 )
-@IF "%SolutionFilename%" == "" (
-	@echo Solution filename unset!
-	@SET ConfigSuccess=0
+IF "%SolutionFilename%" == "" (
+	echo Solution filename unset!
+	SET ConfigSuccess=0
 )
 
-@SET RunDocker=docker run ^
+SET RunDocker=docker run ^
     -v "%DeployRoot%:/ads-deploy/tools" ^
     -v "%SolutionDir%:%IocMountPath%" ^
 	-e DISPLAY=host.docker.internal:0.0 ^
     -i
 
-@SET AdsDeployConfigured=1
+SET AdsDeployConfigured=1
