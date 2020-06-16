a564661c331b2f410d0bf72e4901f297a75f91bc
K Lauer <klauer@slac.stanford.edu>
2020-06-16 13:52:34 -0700

    MNT: more user-friendly switch tools

--- switch_to_conda.cmd
+++ switch_to_conda.cmd
@@ -1,1 +1,6 @@
+@echo off
+echo Using conda for ads-deploy integration in Visual Studio.
+echo Please ensure that you install conda and run setup_conda_on_windows.cmd
+echo before trying to use it.
+
 echo SET AdsDeployUseDocker=0 > tools\select_conda_or_docker.cmd
--- switch_to_docker.cmd
+++ switch_to_docker.cmd
@@ -1,1 +1,4 @@
+@echo off
+echo Using docker for ads-deploy integration in Visual Studio.
 echo SET AdsDeployUseDocker=1 > tools\select_conda_or_docker.cmd
+pause
