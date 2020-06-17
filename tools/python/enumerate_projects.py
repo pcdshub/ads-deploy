f511dfc1289d07fc5bafd0635b70a8d3fb43f05a
K Lauer <klauer@slac.stanford.edu>
2020-06-17 10:01:54 -0700

    CLN: remove unused scripts

--- tools/python/enumerate_projects.py
+++
@@ -1,7 +0,0 @@
-from runpy import run_path
-
-import pathlib
-
-settings = run_path("deploy_config.py")
-for project in settings['projects']:
-    print('export PROJECT=', pathlib.Path(project).absolute())
